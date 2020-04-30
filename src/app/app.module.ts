8ba0499be8fa2af93cf7a00e8f306d3f54204297
zhangguolong <zhangguolong@zzjbim.com>
2020-04-30 11:42:40 +0800

    路由 & 管理数据

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,24 +9,34 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
-    ])    
-  ],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent,
-    ProductAlertsComponent,
-    ProductDetailsComponent
-  ],
-  bootstrap: [ AppComponent ]
+   imports: [
+      BrowserModule,
+      CommonModule,
+      HttpClientModule,
+      ReactiveFormsModule,
+      RouterModule.forRoot([
+         { path: '', component: ProductListComponent },
+         { path: 'products/:productId', component: ProductDetailsComponent },
+         { path: 'cart', component: CartComponent },
+         { path: 'shipping', component: ShippingComponent}
+      ])  
+   ],
+   declarations: [
+      AppComponent,
+      TopBarComponent,
+      ProductListComponent,
+      ProductAlertsComponent,
+      ProductDetailsComponent,
+      CartComponent,
+      ShippingComponent
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
